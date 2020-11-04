6faee3ca97eae7ddfaae6fd3de22682bee19121d
TommyGONG08 <103423198@qq.com>
2020-11-04 02:23:36 -0800

    remove some files

--- build/color_detection/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ghl/Desktop/color_detect_ws/src/color_detection
-# Build directory: /home/ghl/Desktop/color_detect_ws/build/color_detection
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
