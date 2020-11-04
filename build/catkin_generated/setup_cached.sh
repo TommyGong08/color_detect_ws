95114c76da8faaa0c3652c0093d3257d88af7b6d
TommyGONG08 <103423198@qq.com>
2020-11-04 02:13:47 -0800

    add src

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ghl/Desktop/color_detect_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/ghl/Desktop/color_detect_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ghl/Desktop/color_detect_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/ghl/Desktop/color_detect_ws/src:$ROS_PACKAGE_PATH"
---
+++ build/color_detection/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ghl/Desktop/color_detect_ws/src/color_detection
+# Build directory: /home/ghl/Desktop/color_detect_ws/build/color_detection
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
